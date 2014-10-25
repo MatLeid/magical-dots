60e8b1d3b7930f79cdf30faf496fc7d6b5e3a6b7
donmatheo <mathias@donmatheo.de>
2014-10-25 11:51:50 +0200

    replaced manual zooming with simple automatic zooming / scaling

--- core/src/de/donmatheo/game/UnstableRelations.java
+++ core/src/de/donmatheo/game/UnstableRelations.java
@@ -2,14 +2,10 @@ package de.donmatheo.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
@@ -18,35 +14,30 @@ import java.util.Random;
 
 public class UnstableRelations extends ApplicationAdapter {
     private ShapeRenderer shapeRenderer;
+    private Vector3 touchPos;
+    private OrthographicCamera camera;
+    private Vector2 initialDotDimensions;
+
     private Array<Dot> dots = new Array<Dot>();
     private Color yellow = new Color(157, 162, 0, 255);
     private Color blue = new Color(0, 162, 232, 255);
-    private Vector3 touchPos;
-    private OrthographicCamera camera;
+
     private int selectedDot = -1;
     private float offsetX;
     private float offsetY;
-    private InputMultiplexer inputMultiplexer;
-    private MyInputProcessor inputProcessor;
-    private MyGestureHandler gestureHandler;
-
-    public float zoom = 1.0f;
-
-
+    private float zoom = 1.0f;
 
     @Override
     public void create() {
-
-        initaliseInputProcessors();
 
         // setup camera and shapeRenderer
         float screen_width = Gdx.graphics.getWidth();
         float screen_height = Gdx.graphics.getHeight();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, screen_width, screen_height);
+        touchPos = new Vector3();
         shapeRenderer = new ShapeRenderer();
         Random random = new Random();
-
 
         // create list of dots
         int numberOfDots = 5;
@@ -69,10 +60,10 @@ public class UnstableRelations extends ApplicationAdapter {
 
         // random layout of dots
         for (Dot dot : dots) {
-            dot.x = 35 + random.nextInt((int) camera.viewportWidth - 70);
-            dot.y = 35 + random.nextInt((int) camera.viewportHeight - 70);
+            dot.x = dot.radius + random.nextInt((int) (camera.viewportWidth - 2 * dot.radius));
+            dot.y = dot.radius + random.nextInt((int) (camera.viewportHeight - 2 * dot.radius));
         }
-
+        initialDotDimensions = calculateDotsDimension();
     }
 
     @Override
@@ -85,7 +76,7 @@ public class UnstableRelations extends ApplicationAdapter {
 
         // drag touched dot
         if (Gdx.input.isTouched()) {
-            touchPos = new Vector3();
+
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
 
@@ -102,7 +93,7 @@ public class UnstableRelations extends ApplicationAdapter {
                 }
             }
         }
-
+        autoscale();
         camera.zoom = zoom;
         camera.update();
 
@@ -137,18 +128,46 @@ public class UnstableRelations extends ApplicationAdapter {
         shapeRenderer.end();
     }
 
+    public void autoscale() {
+        float zoomX = 1.0f / initialDotDimensions.x * calculateDotsDimension().x;
+        float zoomY = 1.0f / initialDotDimensions.y * calculateDotsDimension().y;
 
-    public void initaliseInputProcessors() {
+        if (zoomX > zoomY) {
+            zoom = zoomX;
+        } else {
+            zoom = zoomY;
+        }
+        if (zoom > 3) {
+            zoom = 3;
+        }
+        if (zoom < 0.5) {
+            zoom = 0.5f;
+        }
 
-        inputMultiplexer = new InputMultiplexer();
+    }
 
-        Gdx.input.setInputProcessor(inputMultiplexer);
-
-        inputProcessor = new MyInputProcessor();
-        gestureHandler = new MyGestureHandler();
-
-        inputMultiplexer.addProcessor(new GestureDetector(gestureHandler));
-        inputMultiplexer.addProcessor(inputProcessor);
+    public Vector2 calculateDotsDimension() {
+        float minX = camera.viewportWidth;
+        float maxX = 0;
+        for (Dot dot : dots) {
+            if (dot.x < minX) {
+                minX = dot.x;
+            }
+            if (dot.x > maxX) {
+                maxX = dot.x;
+            }
+        }
+        float minY = camera.viewportHeight;
+        float maxY = 0;
+        for (Dot dot : dots) {
+            if (dot.y < minY) {
+                minY = dot.y;
+            }
+            if (dot.y > maxY) {
+                maxY = dot.y;
+            }
+        }
+        return new Vector2(maxX - minX, maxY - minY);
     }
 
     @Override
@@ -157,114 +176,4 @@ public class UnstableRelations extends ApplicationAdapter {
     }
 
 
-    class MyInputProcessor implements InputProcessor {
-        @Override
-        public boolean keyDown(int keycode) {
-            return false;
-        }
-
-        @Override
-        public boolean keyUp(int keycode) {
-            return false;
-        }
-
-        @Override
-        public boolean keyTyped(char character) {
-            return false;
-        }
-
-        @Override
-        public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-            return false;
-        }
-
-        @Override
-        public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-            return false;
-        }
-
-        @Override
-        public boolean touchDragged(int screenX, int screenY, int pointer) {
-            return false;
-        }
-
-        @Override
-        public boolean mouseMoved(int screenX, int screenY) {
-            return false;
-        }
-
-        @Override
-        public boolean scrolled(int amount) {
-
-            //Zoom out
-            if (amount > 0 && zoom < 2) {
-                zoom += 0.1f;
-            }
-
-            //Zoom in
-            if (amount < 0 && zoom > 1.1) {
-                zoom -= 0.1f;
-            }
-
-            return true;
-        }
-
-    }
-
-
-    class MyGestureHandler implements GestureDetector.GestureListener {
-
-        public float initialScale = 1.0f;
-
-        @Override
-        public boolean tap(float x, float y, int count, int button) {
-            return false;
-        }
-
-        @Override
-        public boolean longPress(float x, float y) {
-            return false;
-        }
-
-        @Override
-        public boolean fling(float velocityX, float velocityY, int button) {
-            return false;
-        }
-
-        @Override
-        public boolean pan(float x, float y, float deltaX, float deltaY) {
-            return false;
-        }
-
-        @Override
-        public boolean panStop(float x, float y, int pointer, int button) {
-            return false;
-        }
-
-        @Override
-        public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-            return false;
-        }
-
-        @Override
-        public boolean touchDown(float x, float y, int pointer, int button) {
-
-            initialScale = zoom;
-
-            return false;
-        }
-
-        @Override
-        public boolean zoom(float initialDistance, float distance) {
-
-            //Calculate pinch to zoom
-            float ratio = initialDistance / distance;
-
-            //Clamp range and set zoom
-            zoom = MathUtils.clamp(initialScale * ratio, 1.0f, 2.0f);
-
-            return true;
-        }
-
-    }
 }
