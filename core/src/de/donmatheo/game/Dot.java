e116e8eb729211d585d99f3681f0375c90be6ffb
donmatheo <mathias@donmatheo.de>
2014-11-24 19:46:48 +0100

    Added InputListener

--- core/src/de/donmatheo/game/Dot.java
+++ core/src/de/donmatheo/game/Dot.java
@@ -32,6 +32,19 @@ public class Dot extends Actor {
         pointLight.setActive(false);
         renderer = new ShapeRenderer();
 
+
+        setBounds(0, 0, DEFAULTRADIUS, DEFAULTRADIUS);
+        addListener(new InputListener() {
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("down " + x +  ", " + y);
+                return true;
+            }
+
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("up " + x +  ", " + y);
+            }
+        });
+
     }
 
     public void draw(Batch batch, float parentAlpha) {
