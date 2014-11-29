1ccb6810e079fe759b8ba187caac84cee46b1bc9
donmatheo <mathias@donmatheo.de>
2014-11-29 20:33:45 +0100

    Fixed Android start Class [skip-ci]

--- android/src/de/donmatheo/game/android/AndroidLauncher.java
+++ android/src/de/donmatheo/game/android/AndroidLauncher.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import de.donmatheo.game.GameScreen;
+import de.donmatheo.game.UnstableRelations;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new GameScreen(), config);
+		initialize(new UnstableRelations(), config);
 	}
 }
